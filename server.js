c5661205ff9bd36abaa7b73051e47276994132f3
akash elhance <akashelhance@gmail.com>
2021-04-05 00:42:47 +0530

    Inital Setup done

---
+++ server.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const express= require('express')
+const dotenv = require('dotenv');
+
+
+const connectDB = require('./config/db');
+
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+
+connectDB();
+const app = express()
+
+
+//Bosy parser- To read the data from the user:
+
+app.use(express.json());
+// Mouting the users:
+
+app.use('/', (req,res)=>{
+    res.json({"msg": "The api is running"})
+})
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
+);
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  // server.close(() => process.exit(1));
+});
